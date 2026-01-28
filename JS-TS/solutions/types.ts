a3d7aa7913399c759e42af70df7ddae38944afce
Ilya Biryuk <ilya.biryuk2005@icloud.com>
2026-01-28 15:49:16 +0300

    completed task-01 js-ts

--- JS-TS/solutions/types.ts
+++ JS-TS/solutions/types.ts
@@ -1,9 +1,15 @@
 interface Todo {
-
+    id: number,
+    title: string,
+    description: string,
+    status: TodoStatus,
+    readonly createdAt: Date,
 }
 
 enum TodoStatus {
-
+    PENDING, IN_PROGRESS, COMPLETED
 }
 
-export { Todo, TodoStatus };
+type NewTodo = Omit<Todo, 'id' | 'createdAt'>;
+
+export { Todo, TodoStatus, NewTodo };
