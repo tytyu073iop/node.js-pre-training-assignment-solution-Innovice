e7caa65d1b04515076b9e1210458d4c11b55fd1d
Ilya Biryuk <ilya.biryuk2005@icloud.com>
2026-01-30 20:02:12 +0300

    completed task-03 js-ts

--- JS-TS/solutions/todo-factory.ts
+++ JS-TS/solutions/todo-factory.ts
@@ -2,6 +2,13 @@ import { Todo, NewTodo, TodoStatus } from './types';
 
 let nextId = 1;
 
-export function createTodo(input: NewTodo): Todo {
-  throw new Error('createTodo: not implemented');
+export function createTodo(input: Partial<NewTodo>): Todo {
+  let copy: Todo = { 
+    title: input.title || '',
+    description: input.description || '',
+    status: TodoStatus.PENDING,
+    id: nextId++,
+    createdAt: new Date()
+  };
+  return copy;
 }
