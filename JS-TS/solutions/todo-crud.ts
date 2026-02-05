85dc306bef5d3f86eb5c8f2ea5187e258c1304f3
Ilya Biryuk <ilya.biryuk2005@icloud.com>
2026-02-05 17:54:04 +0300

    completed task-04 js-ts

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,39 @@
 import { Todo } from './types';
 
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+  let arr: Todo[] = JSON.parse(JSON.stringify(state));
+  arr.push(todo);
+  return arr;
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+  let arr: Todo[] = JSON.parse(JSON.stringify(state));
+  let req = arr.find((t) => t.id == id);
+  if (req == null) {
+    throw new Error("Cannot find todo with id");
+  }
+
+  if (update.title != null) {
+    req.title = update.title;
+  }
+  if (update.status != null) {
+    req.status = update.status;
+  }
+  if (update.description != null) {
+    req.description = update.description;
+  }
+  return arr;
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+  if (state.find((t) => t.id == id) == null) {
+    throw new Error("Cannot find todo with id");
+  }
+  let arr = state.filter((t) => t.id != id);
+
+  return arr; 
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+  return state.find((t) => t.id == id);
 }
