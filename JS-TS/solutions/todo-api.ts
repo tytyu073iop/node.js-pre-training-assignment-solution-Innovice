0bd925c2b993795dc249e24dc2d06ed37a63034b
Ilya Biryuk <ilya.biryuk2005@icloud.com>
2026-09-17 00:18:10 +0300

    merge solve

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -1,9 +1,10 @@
 import { InMemoryRepository } from './repository';
 import { Todo, NewTodo } from './types';
+import { createTodo } from './todo-factory';
 
 export class TodoApi {
   private repo = new InMemoryRepository<Todo>();
-
+  
   async getAll(): Promise<Todo[]> {
     await new Promise((resolve) => setTimeout(resolve, 400))
     return this.repo.findAll();
@@ -13,12 +14,12 @@ export class TodoApi {
     await new Promise((resolve) => setTimeout(resolve, 400));
     return this.repo.add(newTodo as Todo);
   }
-
+  
   async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
     await new Promise((resolve) => setTimeout(resolve, 400));
     return this.repo.update(id, update);
   }
-
+  
   async remove(id: number): Promise<void> {
     await new Promise((resolve) => setTimeout(resolve, 400));
     return this.repo.remove(id);
