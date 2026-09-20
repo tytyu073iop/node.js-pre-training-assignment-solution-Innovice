7b07e127e56e4d35052a877a23365f535412a286
Ilya Biryuk <ilya.biryuk2005@icloud.com>
2026-09-20 21:36:44 +0300

    done task 9

--- React-CSS/react-todo-app/src/solutions/task-09/Card.tsx
+++ React-CSS/react-todo-app/src/solutions/task-09/Card.tsx
@@ -71,6 +71,7 @@ import './Card.css';
  * - Keep components focused and single-purpose
  */
 export const Card: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  
   // TODO: Implement the Card component
   // 
   // Requirements:
@@ -97,10 +98,8 @@ export const Card: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   // }
 
   return (
-    <div>
-      {/* TODO: Replace this with your implementation */}
-      <h4>Card Component</h4>
-      <p>Implement component composition with children prop here</p>
+    <div className="card">
+      {children}
     </div>
   );
 }; 
